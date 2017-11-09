842405ce8f95e94820382c8b238bf108ed9d4609
lvdeshi2011@163.com <lvdeshi2011@gmail.com>
2017-11-09 17:52:46 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-    before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
+    before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy,:join,:quit]
     before_action :find_group_and_check_permission, only: [:edit,:update,:destroy]
     def index
         @groups = Group.all
@@ -21,7 +21,6 @@ class GroupsController < ApplicationController
         @posts = @group.posts.recent.paginate(page: params[:page], per_page: 3)
     end
     def edit
-
     end
     def update
         if @group.update(groups_params)
@@ -34,6 +33,31 @@ class GroupsController < ApplicationController
         @group.destroy
         redirect_to groups_path,alert: "Group deleted"
     end
+
+    def join
+        @group = Group.find(params[:id])
+        if !current_user.is_member_of?(@group)
+            current_user.join!(@group)
+            flash[:notice] = "加入本讨论版成功！"
+        else
+            flash[:warning] = "你已经是本讨论版成员了！"
+        end
+        redirect_to group_path(@group)
+    end
+    def quit
+        @group = Group.find(params[:id])
+        if  current_user.is_member_of?(@group)
+            current_user.quit!(@group)
+            flash[:notice] = "已退出本讨论版！"
+        else
+            flash[:warning] = "你不是本讨论版成员，怎么退出"
+        end
+        redirect_to group_path(@group)
+    end
+
+
+
+    private
     def groups_params
         params.require(:group).permit(:title,:description)
     end
